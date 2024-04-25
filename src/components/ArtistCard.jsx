ab3413f41074693d69c33d6c6504f76ff4f0a713
elliottsaruski <elliottxsaruski@gmail.com>
2024-04-25 12:53:04 -0400

    website redesign

--- src/components/ArtistCard.jsx
+++ src/components/ArtistCard.jsx
@@ -1,26 +1,13 @@
 import PropTypes from "prop-types";
-import { RiArrowRightUpFill } from "react-icons/ri";
 import { Link } from "react-router-dom";
-import { motion } from "framer-motion";
 
 function ArtistCard({ name, img, artistID }) {
   return (
     <Link to={artistID}>
-      <motion.div
-        className="artist-card"
-        initial={{
-          textDecorationColor: "transparent",
-          borderColor: "#696969",
-          opacity: 0,
-        }}
-        whileInView={{ opacity: 1, transition: { duration: 1 } }}
-        whileHover={{ borderColor: "#eee5e9" }}>
+      <div className="artist-card">
         <img className="artist-card-img" src={img}></img>
-        <div className="artist-name-wrapper">
-          <h2 className="artist-card-name">{name}</h2>
-          <RiArrowRightUpFill className="link-to-artist-icon" />
-        </div>
-      </motion.div>
+        <h2 className="artist-card-name">{name}</h2>
+      </div>
     </Link>
   );
 }
