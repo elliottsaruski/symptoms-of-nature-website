c661c263f2414f907bfc537cbb23dd7e1f02f70d
elliottsaruski <elliottxsaruski@gmail.com>
2024-05-10 13:18:47 -0400

    Implemented module popup state and styles

--- src/pages/Feed.jsx
+++ src/pages/Feed.jsx
@@ -1,5 +1,4 @@
 import { posts } from "../data/PostData.jsx";
-import Filterbar from "../components/Filterbar";
 import FeedPost from "../components/FeedPost";
 import { Outlet } from "react-router-dom";
 
@@ -8,22 +7,8 @@ function Feed() {
     <div id="feed-wrapper">
       <h2>FeeD</h2>
       <hr />
-      <span>
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quos nihil
-        debitis, saepe distinctio molestias non molestiae tempore dignissimos
-        nulla eveniet, dolor, consequatur tempora enim voluptatem! Velit fuga
-        illum consequatur quae!
-      </span>
-      <span>
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quos nihil
-        debitis, saepe distinctio molestias non molestiae tempore dignissimos
-        nulla eveniet, dolor, consequatur tempora enim voluptatem! Velit fuga
-        illum consequatur quae!
-      </span>
-      <div className="feed-post-img-wrapper"></div>
+
       <div id="posts-wrapper">
-        <Filterbar />
-        <hr />
         {posts.map((post, index) => {
           return (
             <FeedPost
