43fcdd7bc0870ac9b95364fa989355624782a1b9
elliottsaruski <elliottxsaruski@gmail.com>
2024-05-28 16:26:19 -0400

    Updates to mobile styles

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -4,9 +4,11 @@ function AboutPage() {
       <h2 className="home-about-SON-title">sYmptoMs oF NatURe</h2>
       <hr />
       <span>
-        Symptoms of nature is a collective immersed in the experimental phase of
-        creation with media spanning from textile, wearable art, soundscapes,
-        music, digital and hand-drawn media/art, and computer coding.
+        Symptoms of Nature is not just a collective; its an immersion into the
+        depths of creativity. Were a group of artisans, creators, and innovators
+        who thrive on experimentation and exploration. Our canvas knows no
+        bounds, spanning from the tangible world of textile and wearable art to
+        the ethereal realms of soundscapes and digital media.
       </span>
       <div className="home-content-wrapper">
         <div className="about-img-wrapper">
@@ -19,12 +21,16 @@ function AboutPage() {
           <img
             src="https://images.pexels.com/photos/207353/pexels-photo-207353.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
             className="about-img-placeholder"></img>
+          <img
+            src="https://images.pexels.com/photos/2887497/pexels-photo-2887497.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+            className="about-img-placeholder"></img>
         </div>
+        <h3>Our Symphony of Creation:</h3>
+        <h4>Textile Art</h4>
         <span>
-          We strive to provide a space for all to showcase their evolution
-          creatively in new ways, open up new opportunities, and unlock the next
-          step in our thought processes by hosting a multitude of engagements
-          such as workshops, consulting, wellness, and contests.
+          Dive into the rich textures and vibrant colors of our textile
+          creations. From intricate patterns to bold designs, our textile art
+          speaks volumes about our passion for innovation.
         </span>
         <div className="about-img-wrapper">
           <img
@@ -36,7 +42,102 @@ function AboutPage() {
           <img
             src="https://images.pexels.com/photos/69825/sunbeam-fog-autumn-nature-69825.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
             className="about-img-placeholder"></img>
+          <img
+            src="https://images.pexels.com/photos/2887497/pexels-photo-2887497.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+            className="about-img-placeholder"></img>
         </div>
+        <h4>Wearable Art</h4>
+        <span>
+          Express yourself with our avant-garde wearable art pieces. Each
+          garment is a masterpiece, blurring the lines between fashion and
+          artistic expression.
+        </span>
+        <div className="about-img-wrapper">
+          <img
+            src="https://images.pexels.com/photos/298821/pexels-photo-298821.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+            className="about-img-placeholder"></img>
+          <img
+            src="https://images.pexels.com/photos/2887497/pexels-photo-2887497.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+            className="about-img-placeholder"></img>
+          <img
+            src="https://images.pexels.com/photos/69825/sunbeam-fog-autumn-nature-69825.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+            className="about-img-placeholder"></img>
+          <img
+            src="https://images.pexels.com/photos/2887497/pexels-photo-2887497.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+            className="about-img-placeholder"></img>
+        </div>
+        <h4>Soundscapes & Music</h4>
+        <span>
+          Lose yourself in the mesmerizing melodies and immersive soundscapes
+          created by our talented musicians. Let the music take you on a journey
+          through the depths of your imagination.
+        </span>
+        <div className="about-img-wrapper">
+          <img
+            src="https://images.pexels.com/photos/298821/pexels-photo-298821.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+            className="about-img-placeholder"></img>
+          <img
+            src="https://images.pexels.com/photos/2887497/pexels-photo-2887497.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+            className="about-img-placeholder"></img>
+          <img
+            src="https://images.pexels.com/photos/69825/sunbeam-fog-autumn-nature-69825.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+            className="about-img-placeholder"></img>
+          <img
+            src="https://images.pexels.com/photos/2887497/pexels-photo-2887497.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+            className="about-img-placeholder"></img>
+        </div>
+        <h4>Digital & Hand-drawn Media/Art</h4>
+        <span>
+          Explore the intersection of the digital and analog worlds with our
+          captivating digital and hand-drawn artworks. From pixelated wonders to
+          free-flowing sketches, our art knows no limits.
+        </span>
+        <div className="about-img-wrapper">
+          <img
+            src="https://images.pexels.com/photos/298821/pexels-photo-298821.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+            className="about-img-placeholder"></img>
+          <img
+            src="https://images.pexels.com/photos/2887497/pexels-photo-2887497.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+            className="about-img-placeholder"></img>
+          <img
+            src="https://images.pexels.com/photos/69825/sunbeam-fog-autumn-nature-69825.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+            className="about-img-placeholder"></img>
+          <img
+            src="https://images.pexels.com/photos/2887497/pexels-photo-2887497.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+            className="about-img-placeholder"></img>
+        </div>
+        <h4>Computer Coding</h4>
+        <span>
+          Unleash the power of technology with our cutting-edge computer coding
+          projects. From interactive installations to mind-bending algorithms,
+          our coding endeavors push the boundaries of whats possible.
+        </span>
+        <div className="about-img-wrapper">
+          <img
+            src="https://images.pexels.com/photos/298821/pexels-photo-298821.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+            className="about-img-placeholder"></img>
+          <img
+            src="https://images.pexels.com/photos/2887497/pexels-photo-2887497.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+            className="about-img-placeholder"></img>
+          <img
+            src="https://images.pexels.com/photos/69825/sunbeam-fog-autumn-nature-69825.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+            className="about-img-placeholder"></img>
+          <img
+            src="https://images.pexels.com/photos/2887497/pexels-photo-2887497.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+            className="about-img-placeholder"></img>
+        </div>
+        <h4> Join Us on this Creative Odyssey</h4>
+        <span>
+          Whether youre an artist, a dreamer, or simply someone who appreciates
+          the beauty of creation, Symptoms of Nature welcomes you with open
+          arms. Dive into our world of experimentation, innovation, and artistic
+          expression, and together, lets create something truly extraordinary.
+          Are you ready to embrace the symptoms of nature? Lets embark on this
+          creative journey together.
+        </span>
+        <img
+          src="https://images.pexels.com/photos/2887497/pexels-photo-2887497.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+          className="about-img-placeholder"></img>
       </div>
     </div>
   );
