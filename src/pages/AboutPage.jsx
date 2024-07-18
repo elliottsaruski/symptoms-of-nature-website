c6f35844d5dc06c0797ca7d9efd584cd33042a6f
elliottsaruski <elliottxsaruski@gmail.com>
2024-07-17 21:17:05 -0400

    aboutus image update

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -3,19 +3,20 @@ import aboutPageHeaderIMG from "../assets/media/photos/ABOUT/about_us_main_image
 // import aboutPageHeaderSTARS from "../assets/media/photos/ABOUT/stars_light_mode.png";
 import aboutPageHeaderTEXT from "../assets/media/photos/ABOUT/about-us-text.png";
 import AboutUsDropDown from "../components/AboutUsDropDown";
+import { useState } from "react";
 
 function AboutPage() {
+  const [headerImage, setHeaderImage] = useState(aboutPageHeaderIMG);
+
   return (
     <div id="about-wrapper">
       <img src={headerIcon} id="about-icon" alt="" />
-      {/* <div className="main-image"> */}
-      <img src={aboutPageHeaderIMG} id="about-header-img" alt="" />
+      <img src={headerImage} id="about-header-img" alt="" />
       {/* <img src={aboutPageHeaderSTARS} id="about-header-stars" alt="" /> */}
-      {/* </div> */}
       <img src={aboutPageHeaderTEXT} id="about-header-text" alt="" />
 
       {/* INTERACTIVE P DROPDOWN SECTION */}
-      <AboutUsDropDown />
+      <AboutUsDropDown setHeaderImage={setHeaderImage} />
     </div>
   );
 }
